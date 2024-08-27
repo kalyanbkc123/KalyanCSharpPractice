258df755a9fecf884d50f1fdc722203087cf1139
Kalyan-Charkavarthi <123096261+kalyanbkc123@users.noreply.github.com>
2024-08-27 16:58:43 -0700

    initial commit

---
+++ KalyanCSharpPractice/08-27-2024/SingleNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalyanCSharpPractice._08_27_2024
+{
+    public class SingleNumber
+    {
+        public int singleItem(int[] nums)
+        {
+            int res = 0;
+
+            for(int i=0; i < nums.Length; i++)
+            {
+                res ^= nums[i];
+
+            }
+
+            return res;
+        }
+    }
+}
--- KalyanCSharpPractice/Program.cs
+++ KalyanCSharpPractice/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World");
+﻿
+
+using KalyanCSharpPractice._08_27_2024;
+
+public class Program
+{ 
+    static void Main(string[] args) 
+    {
+        // Single Item (or) Number
+        int[] nums = { 1, 1, 2, 2, 3, 3, 4 };
+
+        SingleNumber sm = new SingleNumber();
+        int res = sm.singleItem(nums);
+        Console.WriteLine("The Single Number which is not duplicate is : "+res);
+
+        
+
+    }
+
+}
