4e70bae2d786994c1132ad12f67771d957459a24
Kalyan-Charkavarthi <123096261+kalyanbkc123@users.noreply.github.com>
2024-08-27 17:03:17 -0700

    third commit

---
+++ KalyanCSharpPractice/08-27-2024/NumberOf1Bits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalyanCSharpPractice._08_27_2024
+{
+    public class NumberOf1Bits
+    {
+        public int count(int value)
+        {
+            int count = 0;
+
+            while (value != 0)
+            {
+                value = value & value - 1;
+                count++;
+            }
+
+            return count;
+        
+        }
+    }
+}
--- KalyanCSharpPractice/Program.cs
+++ KalyanCSharpPractice/Program.cs
@@ -7,13 +7,21 @@ public class Program
     static void Main(string[] args) 
     {
         // Single Item (or) Number
+        /*
         int[] nums = { 1, 1, 2, 2, 3, 3, 4 };
 
         SingleNumber sm = new SingleNumber();
         int res = sm.singleItem(nums);
         Console.WriteLine("The Single Number which is not duplicate is : "+res);
+        */
 
-        
+        // Number of 1 Bits
+        NumberOf1Bits num = new NumberOf1Bits();
+        int res1 = num.count(7);
+        Console.WriteLine("The Number of 1 Bits is : "+res1);
+
+
+
 
     }
 
