785e651ed7e18f3bcf85b95d042a613d2afd8944
Kalyan-Charkavarthi <123096261+kalyanbkc123@users.noreply.github.com>
2024-08-28 17:29:52 -0700

    commit

---
+++ KalyanCSharpPractice/08-28-2024/MissingNo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalyanCSharpPractice._08_28_2024
+{
+   public class MissingNo
+    {
+            public int missingNumber(int[] nums)
+            {
+
+                int sum = 0;
+                int res = 0;
+                int n = nums.Length;
+
+                for (int i = 0; i <= n; i++)
+                {
+                    sum ^= i;
+                }
+                for (int i = 0; i < n; i++)
+                {
+
+                    res ^= nums[i];
+                }
+                res = res ^ sum;
+                return res;
+
+            }
+        
+
+
+    }
+}
---
+++ KalyanCSharpPractice/08-28-2024/ReverseBits.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalyanCSharpPractice._08_28_2024
+{
+    public class ReverseBits
+    {    
+            public int reverseBits(int n)
+            {
+
+                int result = 0;
+
+                for (int i = 0; i < 32; i++)
+                {
+                    int bit = (n >> i) & 1;
+                    result += (bit << (31 - i));
+                }
+
+                return result;
+
+            }       
+
+        static void Main(string[] args)
+        { 
+            
+        
+        }
+    }
+}
