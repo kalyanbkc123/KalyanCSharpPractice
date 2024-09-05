0456f78fc51a0ba8bf280d6b32c982400271b179
Kalyan-Charkavarthi <123096261+kalyanbkc123@users.noreply.github.com>
2024-09-04 18:03:15 -0700

    commit

---
+++ KalyanCSharpPractice/09-04-2024/ClimbingStairs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalyanCSharpPractice._09_04_2024
+{
+    internal class ClimbingStairs
+    {
+            public int climbStairs(int n)
+            {
+                int a = 1;
+                int b = 1;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    int temp = a + b;
+                    a = b;
+                    b = temp;
+                }
+                return b;
+            }
+        
+
+
+    }
+}
---
+++ KalyanCSharpPractice/09-04-2024/MinCostClimbingStairs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalyanCSharpPractice._09_04_2024
+{
+    internal class MinCostClimbingStairs
+    {
+
+        
+        public int minCostClimbingStairs(int[] cost)
+        {
+            for (int i = cost.length - 3; i >= 0; i--)
+            {
+                cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+            }s
+            return Math.min(cost[0], cost[1]);
+        }
+    
+
+    }
+}
