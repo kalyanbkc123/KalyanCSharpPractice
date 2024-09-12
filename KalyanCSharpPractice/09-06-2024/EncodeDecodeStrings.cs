1dfb1fc2e6b098bfd093c453896215715b927fd8
Kalyan-Charkavarthi <123096261+kalyanbkc123@users.noreply.github.com>
2024-09-12 12:36:51 -0700

    commit

---
+++ KalyanCSharpPractice/09-06-2024/EncodeDecodeStrings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalyanCSharpPractice._09_06_2024
+{
+    public class EncodeDecodeStrings
+    {
+
+        public static string Encode(List<string> strs)
+        {
+            return string.Concat(strs.SelectMany(s => $"{s.Length}#{s}"));
+        }
+
+        public static List<string> Decode(string s)
+        {
+            var res = new List<string>();
+
+            var i = 0;
+            while( i < s.Length)
+            {
+                var j = i;
+                while (s[j] != '#')
+                {
+                    ++j;
+                }
+
+                int.TryParse(s.Substring(i, j - i), out var len);
+                j++;
+
+                res.Add(s.Substring(j, len));
+                i = j + len;
+
+
+            }
+           
+            return  res;
+
+           
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> arr =new List<string>() { "123", "Hia", "How r you"};
+
+            var res = Encode(arr);
+
+            var res1 = Decode(res);
+
+            foreach(var x in arr)
+            {   
+                Console.WriteLine(x);
+            }
+        }
+       
+    }
+}
---
+++ KalyanCSharpPractice/09-06-2024/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalyanCSharpPractice._09_06_2024
+{
+    public class ProductOfArrayExceptSelf
+    {
+
+        public static int[] ProductArray(int[] numbers)
+        {
+            int prefix = 1, postfix =1 ;
+
+            int[] res = new int[numbers.Length];
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                res[i] = prefix;
+                prefix *= numbers[i];
+
+            }
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                res[i] *= postfix;
+                postfix *= numbers[i];
+
+            }
+
+            return res;
+        }
+
+        public static void Main(string[] args)
+        {
+            var arr = new int[]{ 1, 2, 3, 4 };
+
+            var res = ProductArray(arr);
+
+            foreach(var n in res)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
